3e79bb118fa3ccbcef3eb1723f52aaf374108fd9
schips <schips@dingtalk.com>
2022-02-17 15:30:17 +0800

    mqtt : 优化编译脚本

--- mqtt/make.sh
+++ mqtt/make.sh
@@ -7,51 +7,15 @@
 #!/bin/sh
 source ../.common
 
-OPENSSL=openssl-1.0.2t
-
-download_package () {
-    cd ${BASE}/compressed
-    #下载包
-    tget  https://www.openssl.org/source/${OPENSSL}.tar.gz
+download_mqtt () {
+    get_ssl
     tgit  https://github.com/eclipse/paho.mqtt.c.git
     tgit  https://github.com/eclipse/mosquitto.git
 }
 
-pre_make_ssl () {
-    cd ${BASE}/source/${OPENSSL}
-    startLine=`sed -n '/install_html_docs\:/=' Makefile`
-    echo $startLine
-    # 为了避免 多行结果
-    for startline in $startLine
-    do
-        lineAfter=99
-        endLine=`expr $startline + 999`
-        sed -i $startline','$endLine'd' Makefile
-        echo "install_html_docs:" >> Makefile
-        echo "\t@echo skip by Schips" >> Makefile
-        echo "install_docs:" >> Makefile
-        echo "\t@echo skip by Schips" >> Makefile
-        echo "# DO NOT DELETE THIS LINE -- make depend depends on it." >> Makefile
-        break
-    done
-}
-
-make_ssl () {
-    # 编译安装 zlib
-    cd ${BASE}/source/${OPENSSL}
-    rm ${OUTPUT_PATH}/${OPENSSL} -rf
-    echo "SSL ABOUT"
-    CC=${_CC} ./Configure no-asm shared --prefix=${OUTPUT_PATH}/${OPENSSL}
-
-    sed 's/-m64//g'  -i Makefile # 删除-m64 关键字 (arm-gcc 不支持)
-    #sudo mv /usr/bin/pod2man /usr/bin/pod2man_bak
-    #mv doc/apps /tmp/
-    pre_make_ssl
-    make clean
-    make $MKTHD && make install
-}
-
 make_paho_mqtt_c () {
+    make_ssl || return 1
+    bash <<EOF
     cd ${BASE}/source/paho.mqtt.c
 
     make $MKTHD CFLAGS+="-I ${OUTPUT_PATH}/${OPENSSL}/include" \
@@ -65,61 +29,64 @@ make_paho_mqtt_c () {
     mkdir ${OUTPUT_PATH}/paho.mqtt.c/bin -p
     mkdir ${OUTPUT_PATH}/paho.mqtt.c/include -p
 
-    cp ${BASE}/source/paho.mqtt.c/build/output/lib*    ${OUTPUT_PATH}/paho.mqtt.c/lib -d -v
-    cp ${BASE}/source/paho.mqtt.c/build/output/samples ${OUTPUT_PATH}/paho.mqtt.c/bin -r -v
-    cp ${BASE}/source/paho.mqtt.c/build/output/test    ${OUTPUT_PATH}/paho.mqtt.c/bin -r -v
-    cp ${BASE}/source/paho.mqtt.c/src/*.h       ${OUTPUT_PATH}/paho.mqtt.c/include    -v
-
+    cp -d -v ${BASE}/source/paho.mqtt.c/build/output/lib*    ${OUTPUT_PATH}/paho.mqtt.c/lib
+    cp -r -v ${BASE}/source/paho.mqtt.c/build/output/samples ${OUTPUT_PATH}/paho.mqtt.c/bin
+    cp -r -v ${BASE}/source/paho.mqtt.c/build/output/test    ${OUTPUT_PATH}/paho.mqtt.c/bin
+    cp    -v ${BASE}/source/paho.mqtt.c/src/*.h              ${OUTPUT_PATH}/paho.mqtt.c/include
+EOF
 }
 
 make_mosquitto () {
+    bash <<EOF
     cd ${BASE}/source/mosquitto
 
-    sed -r -i "/WITH_DOCS:=/ s/.*/WITH_DOCS:=no/"                       config.mk
-    sed -r -i "/WITH_SRV:=/ s/.*/WITH_SRV:=no/"                         config.mk
-    sed -r -i "/WITH_UUID:=/ s/.*/WITH_UUID:=no/"                       config.mk
-    sed -r -i "/WITH_WEBSOCKETS:=/ s/.*/WITH_WEBSOCKETS:=no/"           config.mk
-    sed -r -i "/WITH_STRIP:=/ s/.*/WITH_STRIP:=yes/"                    config.mk
+    # 杂项
+    sed -r -i "/WITH_DOCS:=/ s/.*/WITH_DOCS:=no/"                   config.mk
+    sed -r -i "/WITH_SRV:=/ s/.*/WITH_SRV:=no/"                     config.mk
+    sed -r -i "/WITH_UUID:=/ s/.*/WITH_UUID:=no/"                   config.mk
+    sed -r -i "/WITH_WEBSOCKETS:=/ s/.*/WITH_WEBSOCKETS:=no/"       config.mk
+    sed -r -i "/WITH_STRIP:=/ s/.*/WITH_STRIP:=yes/"                config.mk
     # 不添加 ssl 支持
-    sed -r -i "/WITH_TLS:=/ s/.*/WITH_TLS:=no/"                         config.mk
-    sed -r -i "/WITH_TLS_PSK:=/ s/.*/WITH_TLS_PSK:=no/"                 config.mk
+    sed -r -i "/WITH_TLS:=/ s/.*/WITH_TLS:=no/"                     config.mk
+    sed -r -i "/WITH_TLS_PSK:=/ s/.*/WITH_TLS_PSK:=no/"             config.mk
+    # 不添加 cjson支持
+    sed -r -i "/WITH_CJSON:=/ s/.*/WITH_CJSON:=no/"                 config.mk
 
     # 指定工具链
-    sed -i "N;2aCC=${BUILD_HOST}gcc"                                     config.mk
-    sed -i "N;2aCXX=${BUILD_HOST}g++"                                    config.mk
-    sed -i "N;2aCPP=${BUILD_HOST}g++"                                    config.mk
-    sed -i "N;2aCC=${BUILD_HOST}gcc"                                     config.mk
-    sed -i "N;2aAR=${BUILD_HOST}ar"                                      config.mk
-    sed -i "N;2aLD=${BUILD_HOST}ld"                                      config.mk
-    sed -r -i "/STRIP\?=/ s/.*/STRIP\?=${BUILD_HOST}strip/"     config.mk
+    sed -i "N;2aCC=${_CC} "                             config.mk
+    sed -i "N;2aCXX=${_CPP} "                           config.mk
+    sed -i "N;2aCPP=${_CPP} "                           config.mk
+    sed -i "N;2aAR=${_AR} "                             config.mk
+    sed -i "N;2aLD=${_LD} "                             config.mk
+    sed -r -i "/STRIP\?=/ s/.*/STRIP\?=${_STRIP}/"     config.mk
     # 设置输出路径
-    echo "DESTDIR=${OUTPUT_PATH}/mosquitto"                 >>  config.mk
-    echo "prefix=\\" >>  config.mk
-
+    echo "DESTDIR=${OUTPUT_PATH}/mosquitto"        >>  config.mk
+    echo 'prefix=\\' >>  config.mk
     make clean
+
     make $MKTHD && make install
 
     # 添加 ssl 支持 下编译的 mosquitto
-   #sed -r -i "/WITH_TLS:=/ s/.*/WITH_TLS:=yes/"                        config.mk
-   #sed -r -i "/WITH_TLS_PSK:=/ s/.*/WITH_TLS_PSK:=yes/"                config.mk
-   #make \
-   #    CFLAGS+="-I ${OUTPUT_PATH}/${OPENSSL}/include" \
-   #    LDFLAGS+="-L${OUTPUT_PATH}/${OPENSSL}/lib -lssl -lcrypto"
+    #sed -r -i "/WITH_TLS_PSK:=/ s/.*/WITH_TLS_PSK:=yes/"                config.mk
+    #sed -r -i "/WITH_TLS:=/ s/.*/WITH_TLS:=yes/"                        config.mk
+    #make \
+    #    CFLAGS+="-I ${OUTPUT_PATH}/${OPENSSL}/include" \
+    #    LDFLAGS+="-L${OUTPUT_PATH}/${OPENSSL}/lib -lssl -lcrypto"
 
-   #make \
-   #    CFLAGS+="-I ${OUTPUT_PATH}/${OPENSSL}/include" \
-   #    LDFLAGS+="-L${OUTPUT_PATH}/${OPENSSL}/lib -lssl -lcrypto" \
-   #    install
+    #make \
+    #    CFLAGS+="-I ${OUTPUT_PATH}/${OPENSSL}/include" \
+    #    LDFLAGS+="-L${OUTPUT_PATH}/${OPENSSL}/lib -lssl -lcrypto" \
+    #    install
+    #
+EOF
 }
 
 function make_build ()
 {
-    download_package  || return 1
+    download_mqtt  || return 1
     tar_package || return 1
-    make_ssl || return 1
     make_paho_mqtt_c || return 1
     make_mosquitto || return 1
-
 }
 
 make_build || echo "Err"
