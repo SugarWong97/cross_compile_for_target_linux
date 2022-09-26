9786e9c1c5c62105ccd796369bbabd5ecea68d35
schips <schips@dingtalk.com>
2022-09-26 19:23:52 +0800

    lrzsz : 去除对libnsl的依赖

--- lrzsz/make.sh
+++ lrzsz/make.sh
@@ -13,18 +13,35 @@ source ../.common
 LRZSZ=0.12.20
 LRZSZ_INSTALL=${OUTPUT_PATH}/lrzsz
 
+LIBNSL=2.0.0
+LIBNSL_INSTALL=${OUTPUT_PATH}/libnsl
+
 download_package () {
     cd ${BASE}/compressed
     tget https://ohse.de/uwe/releases/lrzsz-${LRZSZ}.tar.gz
+    #tget https://github.com/thkukuk/libnsl/releases/download/v${LIBNSL}/libnsl-${LIBNSL}.tar.xz
+}
+
+make_nsl () {
+    cd $CODE_PATH/libnsl-${LIBNSL}
+
+    ./configure --host=${BUILD_HOST} --prefix=${LIBNSL_INSTALL} \
+        --disable-nsl \
+        --enable-static --disable-shared
+
+    make CC=${_CC} prefix=${LIBNSL_INSTALL} || return -1
+
+    make install
+
 }
 
 make_lrzsz () {
 
     cd $CODE_PATH/lrzsz-${LRZSZ}
 
-    ./configure --host=${BUILD_HOST} --prefix=${LRZSZ_INSTALL}
+    ./configure --host=${BUILD_HOST} --prefix=${LRZSZ_INSTALL} --disable-nls LIBS=""
 
-    make CC=${_CC} prefix=${LRZSZ_INSTALL} || return -1
+    make CC=${_CC} prefix=${LRZSZ_INSTALL}  LIBS="" || return -1
 
     make install
 }
@@ -34,6 +51,7 @@ function make_build ()
     download_package  || return 1
     tar_package || return 1
 
+    #make_nsl || return 1
     make_lrzsz  || return 1
 }
 
