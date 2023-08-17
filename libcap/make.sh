ff7c8ce74e54091de8b78bfa463f277211b1b2b0
schips <schips@dingtalk.com>
2023-08-17 15:13:52 +0800

    libcap : 新增 v2.67 版本的移植文件
    命令写在.common中

---
+++ libcap/make.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##
+#    Copyright By Schips, All Rights Reserved
+#    https://gitee.com/schips/
+
+#    File Name:  make.sh
+#    Created  :  2023-08-17 15:09:10 PM CST
+
+##
+
+source ../.common
+
+
+
+make_libcap
