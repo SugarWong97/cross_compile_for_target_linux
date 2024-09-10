0442ecc0dbe934d8d44a4b44a21dc96b6fba355f
schips <schips@dingtalk.com>
2024-09-10 16:47:58 +0800

    zlib : 规范编译脚本

--- .common_lib/zlib.sh
+++ .common_lib/zlib.sh
@@ -1,14 +1,19 @@
-export ZLIB=zlib
+ZLIB=zlib
 export CONFIG_ZLIB_VERSION=1.2.11
 export ZLIB_VERSION=${ZLIB}-${CONFIG_ZLIB_VERSION}
 
+export ZLIB_OUTPUT_PATH=${OUTPUT_PATH}/${ZLIB}
 
 ## for others
-ZLIB_FILE_NAME=${ZLIB_VERSION}.tar.gz
-ZLIB_ARCH_PATH=$ROOT_DIR/zlib/compressed/${ZLIB_FILE_NAME}
+export ZLIB_FILE_NAME=${ZLIB_VERSION}.tar.gz
+export ZLIB_ARCH_PATH=$ROOT_DIR/zlib/compressed/${ZLIB_FILE_NAME}
 
 ### ZLIB
 function get_zlib () {
+    export ZLIB_VERSION=${ZLIB}-${CONFIG_ZLIB_VERSION}
+    export ZLIB_FILE_NAME=${ZLIB_VERSION}.tar.gz
+    export ZLIB_ARCH_PATH=$ROOT_DIR/zlib/compressed/${ZLIB_FILE_NAME}
+
     if [ -f "$ZLIB_ARCH_PATH" ]; then
         mkdir -p $ARCHIVE_PATH
         mk_softlink_to_dest $ZLIB_ARCH_PATH $ARCHIVE_PATH/$ZLIB_FILE_NAME
@@ -19,15 +24,17 @@ function get_zlib () {
 }
 
 function mk_zlib () {
+    export ZLIB_VERSION=${ZLIB}-${CONFIG_ZLIB_VERSION}
 bash <<EOF
     cd ${CODE_PATH}/${ZLIB_VERSION}
-    CC=${_CC} ./configure --prefix=${OUTPUT_PATH}/${ZLIB}
+    CC=${_CC} ./configure --prefix=${ZLIB_OUTPUT_PATH}
     make clean
     make $MKTHD && make install
 EOF
 }
 
 function make_zlib () {
+    export ZLIB_VERSION=${ZLIB}-${CONFIG_ZLIB_VERSION}
     get_zlib
     tar_package       || return 1
     mk_zlib
--- zlib/make.sh
+++ zlib/make.sh
@@ -2,4 +2,7 @@
 
 source ../.common
 
-make_zlib || echo "Err"
+#export CONFIG_ZLIB_VERSION=1.2.11
+#export ZLIB_OUTPUT_PATH=${OUTPUT_PATH}/${ZLIB}
+
+make_zlib
