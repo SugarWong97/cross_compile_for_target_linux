7f4d49b805d4b8e4b28c13515575dbcce7b96552
schips <schips@dingtalk.com>
2024-09-10 13:34:48 +0800

    ntp : 允许在编译脚本中指定可配置项

--- .common_lib/ntp.sh
+++ .common_lib/ntp.sh
@@ -3,6 +3,18 @@ export NTP=ntp
 export CONFIG_NTP_VERSION=4.2.8p17
 export NTP_VERSION=${NTP}-${CONFIG_NTP_VERSION}
 
+#export CONFIG_NTP_SERVER_IP
+#export CONFIG_NTP_OTHER_SERVER_IP
+#export CONFIG_NTP_SERVER_DRIFTFILE
+#export CONFIG_NTP_SERVER_PIDFILE
+#export CONFIG_NTP_SERVER_LOGFILE
+
+export CONFIG_NTP_SERVER_IP_DEFAULT="192.168.1.1"
+export CONFIG_NTP_OTHER_SERVER_IP_DEFAULT="192.168.1.2"
+export CONFIG_NTP_SERVER_DRIFTFILE_DEFAULT="/var/ntp/drift"
+export CONFIG_NTP_SERVER_PIDFILE_DEFAULT="/var/run/ntpd.pid"
+export CONFIG_NTP_SERVER_LOGFILE_DEFAULT="/var/log/ntp.log"
+
 NTP_OUTPUT=${OUTPUT_PATH}/${NTP}
 
 #下载包
@@ -10,9 +22,9 @@ download_ntp () {
     get_ssl
     #echo "https://downloads.nwtime.org/ntp/"
 
-    # 4.2.8p17 -> 4.2.8
     # e.g :
     ##  https://downloads.nwtime.org/ntp/4.2.8/ntp-4.2.8p17.tar.gz
+    ##  4.2.8p17 -> 4.2.8
     config_ntp_version_for_url=`echo $CONFIG_NTP_VERSION| cut -f 1 -d "p" `
 
     tget  https://downloads.nwtime.org/ntp/${config_ntp_version_for_url}/${NTP_VERSION}.tar.gz
@@ -50,78 +62,128 @@ gen_ntp_client_usage()
 {
 (
     cat<<EOF
-1. 修改时区(可选)
-  export TZ="UTC-08:00"
+# 修改时区(可选)
+#export TZ="UTC-08:00"
 
-2. 同步时间服务器的时间
-  ./bin/ntpdate   target-ip
-  例如 ：
-    ./bin/ntpdate   time.buptnet.edu.cn
+# 同步时间服务器的时间
+killall ntpdate > /dev/null 2>&1 && sleep 1
+./bin/ntpdate   $CONFIG_NTP_SERVER_IP
+#./bin/ntpdate   time.buptnet.edu.cn
 
-3. 显示UTC时间可以使用命令
-    date -u
+# 显示UTC时间
+date -u
 
-4. 保存时间到本地（如果有硬件RTC）
-    hwclock b-w
+# 保存时间到本地（如果有硬件RTC）
+hwclock -w
 EOF
-)   > ${NTP_OUTPUT}/client.txt
+)   > ${NTP_OUTPUT}/client.sh
+    chmod + x ${NTP_OUTPUT}/client.sh
+
+(
+    cat<<EOF
+while true
+do
+    ./client.sh
+    echo "Waiting 600s for next update"
+    sleep 600
+done
+EOF
+)   > ${NTP_OUTPUT}/client_loop.sh
+    chmod + x ${NTP_OUTPUT}/client_loop.sh
+
 }
 
 gen_ntp_server_usage()
 {
-    driftfile="/var/ntp/drift"
-    server_ip="192.168.1.1"
-
+    network_ip=`echo ${CONFIG_NTP_SERVER_IP%.*}.0`
 
 (
     cat<<EOF
-# 注意
-# ntp服务器开启后五到十几分钟才能在客户端系统中执行以下命令，否则时间同步会失败
+#配置：为同网络的其他设备提供NTP服务，无上级NTP-Server连接
+## 注意：ntp服务器开启后五到十几分钟才能在客户端系统中执行以下命令，
+## 否则时间同步会失败，提示'no server suitable for synchronization found'
 
-# 指定时间漂移记录文件，
+# 指定时间漂移记录文件
 # 作用：如果ntpd停止并重新启动，它将从该文件初始化频率，并避免可能的长时间间隔重新学习校正。
-driftfile ${driftfile}
+driftfile ${CONFIG_NTP_SERVER_DRIFTFILE}
 
-##下面两行默认是拒绝所有来源的任何访问
-restrict default kod nomodify notrap nopeer noquery
-restrict -6 default kod nomodify notrap nopeer noquery
+pidfile   ${CONFIG_NTP_SERVER_PIDFILE}
+logfile   ${CONFIG_NTP_SERVER_LOGFILE}
 
-#开放本机的任何访问
+### 拒绝所有来源的任何访问(未启用)
+## restrict default kod nomodify notrap nopeer noquery
+## restrict -6 default kod nomodify notrap nopeer noquery
+
+# 开放本机的任何访问
 restrict 127.0.0.1
 restrict -6 ::1
 
-# 允许内网其他机器同步时间(允许192.168.1.0/24 网段主机进行时间同步)
-restrict 192.168.1.0 mask 255.255.255.0 nomodify notrap
+# 允许 ${network_ip}/24 网段主机进行时间同步
+restrict ${network_ip} mask 255.255.255.0 nomodify notrap
 
-#指定ntp服务器地址
-server $server_ip
+# 允许以本地时间作为时间服务
+server   127.127.1.0 # local clock
+# 本地时间的精准度等级
+fudge    127.127.1.0 stratum 5
 
-#允许上层时间服务器主动修改本机时间
-restrict $server_ip nomodify notrap noquery
 
-#外部时间服务器不可用时，以本地时间作为时间服务
-server  127.127.1.0
-fudge   127.127.1.0 stratum 10
+### 允许上层时间服务器主动修改本机时间(目前未成功)
+## restrict $CONFIG_NTP_OTHER_SERVER_IP nomodify notrap noquery
+## server   $CONFIG_NTP_OTHER_SERVER_IP prefer iburst minpoll 4 maxpoll 10
 
-##下面两行为配置文件默认开启
-includefile /etc/ntp/crypto/pw
+
+# 广播延迟
+broadcastdelay 0.008
+# 配置key
 keys /etc/ntp/keys
 
-
+### 引入指定目录下的配置
+## includefile /etc/ntp/crypto/pw
 EOF
-)   > ${NTP_OUTPUT}/server.ntp.conf
+)   > ${NTP_OUTPUT}/ntpd.conf
+
 
 (
     cat<<EOF
-启动server
-./bin/ntpd-c server.ntp.conf  # -p /tmp/ntpd.pid
+# 创建有关文件
+## Drift 文件
+mkdir -p \`dirname  $CONFIG_NTP_SERVER_DRIFTFILE\`
+touch    $CONFIG_NTP_SERVER_DRIFTFILE
+## PID 文件
+mkdir -p \`dirname  $CONFIG_NTP_SERVER_PIDFILE\`
+touch    $CONFIG_NTP_SERVER_PIDFILE
+## LOG 文件
+mkdir -p \`dirname  $CONFIG_NTP_SERVER_LOGFILE\`
+touch    $CONFIG_NTP_SERVER_LOGFILE
+
+# 启动server
+./bin/ntpd -c ntpd.conf
+sleep 2; cat $CONFIG_NTP_SERVER_LOGFILE
 EOF
-)   > ${NTP_OUTPUT}/server.txt
+)   > ${NTP_OUTPUT}/server.sh
+    chmod + x ${NTP_OUTPUT}/server.sh
 
 }
 
 make_ntp ()
 {
+    if [ -z "$CONFIG_NTP_SERVER_IP" ];then
+        export CONFIG_NTP_SERVER_IP="$CONFIG_NTP_SERVER_IP_DEFAULT"
+    fi
+    if [ -z "$CONFIG_NTP_OTHER_SERVER_IP" ];then
+        export CONFIG_NTP_OTHER_SERVER_IP="$CONFIG_NTP_OTHER_SERVER_IP_DEFAULT"
+    fi
+    if [ -z "$CONFIG_NTP_SERVER_DRIFTFILE" ];then
+        export CONFIG_NTP_SERVER_DRIFTFILE="$CONFIG_NTP_SERVER_DRIFTFILE_DEFAULT"
+    fi
+
+    if [ -z "$CONFIG_NTP_SERVER_PIDFILE" ];then
+        export CONFIG_NTP_SERVER_PIDFILE="$CONFIG_NTP_SERVER_PIDFILE_DEFAULT"
+    fi
+    if [ -z "$CONFIG_NTP_SERVER_LOGFILE" ];then
+        export CONFIG_NTP_SERVER_LOGFILE="$CONFIG_NTP_SERVER_LOGFILE_DEFAULT"
+    fi
+
     download_ntp
     tar_package
     make_ssl  || { echo >&2 "make_ssl "; exit 1; }
--- ntp/make.sh
+++ ntp/make.sh
@@ -1,3 +1,18 @@
 source ../.common
 
+## 配置NTP当前服务器IP
+#export CONFIG_NTP_SERVER_IP="192.168.1.1"
+
+## 配置NTP上一层服务器IP(可选)
+#export CONFIG_NTP_OTHER_SERVER_IP="192.168.1.2"
+
+## 配置NTP服务器drift文件路径
+#export CONFIG_NTP_SERVER_DRIFTFILE"/var/ntp/drift"
+
+## 配置NTP服务器PID文件路径
+#export CONFIG_NTP_SERVER_PIDFILE="/var/run/ntpd.pid"
+
+## 配置NTP服务器Log文件路径(建议搭建初期打开)
+#export CONFIG_NTP_SERVER_LOGFILE="/var/log/ntp.log"
+
 make_ntp
