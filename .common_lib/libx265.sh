870294f2f5d3464f15901543fdbb785eac6b87d6
schips <schips@dingtalk.com>
2025-04-16 17:56:47 +0800

    libx265 : 升级为3.5版本
    对aarch64更友好

--- .common_lib/libx265.sh
+++ .common_lib/libx265.sh
@@ -1,20 +1,23 @@
-export X265=x265_3.2
+export X265=x265
+export CONFIG_X265_VERSION=3.5
+export X265_VERSION=x265_${CONFIG_X265_VERSION}
 export X265_OUTPUT_PATH=${OUTPUT_PATH}/x265
 export X265_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/x265
 
 ## for others
-export X265_FILE_NAME=${X265}.tar.gz
+export X265_FILE_NAME=${X265_VERSION}.tar.gz
 export X265_ARCH_PATH=$ROOT_DIR/libx265/compressed/${X265_FILE_NAME}
 
 function _sync_export_var_x265()
 {
-    export X265_FILE_NAME=${X265}.tar.gz
+    export X265_VERSION=x265_${CONFIG_X265_VERSION}
+    export X265_FILE_NAME=${X265_VERSION}.tar.gz
     export X265_ARCH_PATH=$ROOT_DIR/libx265/compressed/${X265_FILE_NAME}
 }
 
 get_x265 () {
     _sync_export_var_x265
-    tget_package_from_arch $X265_ARCH_PATH  $ARCHIVE_PATH/$X265_FILE_NAME http://download.videolan.org/videolan/x265/${X265}.tar.gz
+    tget_package_from_arch $X265_ARCH_PATH  $ARCHIVE_PATH/$X265_FILE_NAME http://download.videolan.org/videolan/x265/${X265_VERSION}.tar.gz
 }
 
 mk_x265() {
@@ -30,18 +33,19 @@ mk_x265() {
         output_dir=${X265_OUTPUT_PATH_HOST}
     fi
 
-    cd ${CODE_PATH}/${X265}/build
+    cd ${CODE_PATH}/${X265_VERSION}/build
+    rm -rf $build_dir
     mkdir -p $build_dir
     cd $build_dir
 
     # 获取 工具链所在位置
     GCC_FULL_PATH=`whereis ${BUILD_HOST_}gcc | awk -F: '{ print $2 }' | awk '{print $1}'` # 防止多个结果
     CROSS_PATH=`dirname ${GCC_FULL_PATH}`
-    touch crosscompile.cmake
+    echo "" >  crosscompile.cmake
 
     if [ -z "$for_host" ];then
-        echo "set(CMAKE_SYSTEM_NAME Linux)" > crosscompile.cmake
-        echo "set(CMAKE_SYSTEM_PROCESSOR arm)" >> crosscompile.cmake
+        echo "set(CMAKE_SYSTEM_NAME Linux)" >> crosscompile.cmake
+        echo "set(CMAKE_SYSTEM_PROCESSOR armv6)" >> crosscompile.cmake
         echo "" >> crosscompile.cmake
         echo "# specify the cross compiler" >> crosscompile.cmake
         echo "set(CMAKE_C_COMPILER ${CROSS_PATH}/${BUILD_HOST}-gcc)" >> crosscompile.cmake
@@ -67,6 +71,7 @@ mk_x265_host () {
 }
 
 function make_x265 () {
+    _sync_export_var_x265
     get_x265
     tar_package
     mk_x265
--- libx265/make.sh
+++ libx265/make.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
 source ../.common
+
+#export CONFIG_X265_VERSION=3.5
 
 #export X265_OUTPUT_PATH=${OUTPUT_PATH}/x265
 #export X265_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/x265
