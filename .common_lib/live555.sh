cacaaee6e93251b729f816c8ad4ea24888228890
schips <schips@dingtalk.com>
2025-04-16 16:16:50 +0800

    live555 : 优化编译脚本，允许指定任意版本

--- .common_lib/live555.sh
+++ .common_lib/live555.sh
@@ -1,39 +1,49 @@
-#!/bin/sh
+export LIVE555=live555
 
-export LIVE555=live555-latest
+#export CONFIG_LIVE555_VERSION=2025.01.17
+export CONFIG_LIVE555_VERSION=latest
+
+export LIVE555_VERSION=${LIVE555}-${CONFIG_LIVE555_VERSION}
+
+export LIVE555_OUTPUT_PATH=${OUTPUT_PATH}/${LIVE555}
 
 function get_live555 () {
-    tget http://www.live555.com/liveMedia/public/${LIVE555}.tar.gz
+    if [ "$CONFIG_LIVE555_VERSION" = "latest" ]; then
+        tget http://www.live555.com/liveMedia/public/${LIVE555_VERSION}.tar.gz
+    else
+        tget https://download.videolan.org/pub/contrib/live555/live.${CONFIG_LIVE555_VERSION}.tar.gz
+    fi
 }
 
 function mk_live555 () {
-    CONFIG_FILE=config.armlinux
-    cd ${CODE_PATH}/live* || return 1
+    cd ${CODE_PATH}/live || return 1
 
-    NEW_LIBS_CONSOLE="-L${OUTPUT_PATH}/${OPENSSL}/lib -lssl -lcrypto"
-
-    #export INCLUDES='$(INCLUDES)'
 read -r -d '' NEW_COPTS <<- EOF
 \$(INCLUDES)   \
 -I. \
--I${OUTPUT_PATH}/${OPENSSL}/include \
--L${OUTPUT_PATH}/${OPENSSL}/lib \
+-IliveMedia/include/ \
+-I${OPENSSL_OUTPUT_PATH}/include \
+-L${OPENSSL_OUTPUT_PATH}/lib \
 -O2 -DSOCKLEN_T=socklen_t -DNO_SSTREAM=1 -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64 \
 -DNO_STD_LIB -lssl -lcrypto
 EOF
 
     ./genMakefiles  armlinux || return 1
 
-    #make clean
+    export NEW_LIBS_CONSOLE="-I${OPENSSL_OUTPUT_PATH}/include -L${OPENSSL_OUTPUT_PATH}/lib -lssl -lcrypto"
+    #bash << EOF #BUG
     export COMPILE_OPTS="${NEW_COPTS}"
     export LIBS_FOR_CONSOLE_APPLICATION="${NEW_LIBS_CONSOLE}"
+    make clean
     make CROSS_COMPILE=${BUILD_HOST_} \
         COMPILE_OPTS="${NEW_COPTS}" \
         LIBS_FOR_CONSOLE_APPLICATION="${NEW_LIBS_CONSOLE}" \
-        $MKTHD && make install PREFIX=${OUTPUT_PATH}/live555
+        $MKTHD && make install PREFIX=${LIVE555_OUTPUT_PATH}
+    #EOF
 }
 
 function make_live555 () {
+    rm ${CODE_PATH}/live -rf
     get_live555
     get_ssl
 
@@ -41,3 +51,4 @@ function make_live555 () {
     make_ssl
     mk_live555
 }
+
--- live555/make.sh
+++ live555/make.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
 source ../.common
+
+# history version (YYYY.MM.DD): https://download.videolan.org/pub/contrib/live555/
+# current version (latest)    : http://www.live555.com/liveMedia/public/
+
+#export CONFIG_LIVE555_VERSION=2025.01.17
+export CONFIG_LIVE555_VERSION=latest
 
 make_live555
