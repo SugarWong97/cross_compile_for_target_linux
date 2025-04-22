db21af7908bc8d44684d2880a0e5ac8949ad6312
schips <schips@dingtalk.com>
2025-04-22 11:07:38 +0800

    ffmpeg : 优化脚本，修复问题
    1. 外部使用时，ffmpeg的包作为软链接
    2. 防止libx264, libx265安装路径被覆盖
    3. 编译libx264时，不再指定ASM选项（由libx264分别尝试编出对应的结果）

--- .common_lib/ffmpeg.sh
+++ .common_lib/ffmpeg.sh
@@ -9,19 +9,23 @@ export FFMPEG_VERSION=${FFMPEG}-${CONFIG_FFMPEG_VERSION}
 export USING_X264_FOR_FFMPEG
 export USING_X265_FOR_FFMPEG
 
+## for others
+export FFMPEG_FILE_NAME=${FFMPEG_VERSION}.tar.bz2
+export FFMPEG_ARCH_PATH=$ROOT_DIR/ffmpeg/compressed/${FFMPEG_FILE_NAME}
+
 # LIBX264 Config(启用libx264时有效)
 ### 通过y/n来配置libx264是否启用ASM（默认禁用）
 #export USING_X264_ASM=n
 ### 通过y/n来配置libx264是否启用OPENCL（默认禁用）
 #export USING_X264_OPENCL=n
 
-export X264_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
-export X265_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
 export FFMPEG_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
+#export X264_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
+#export X265_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
 
-export X264_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
-export X265_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
 export FFMPEG_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
+#export X264_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
+#export X265_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
 
 set_ffmpeg()
 {
@@ -40,7 +44,7 @@ set_ffmpeg()
     export FFMPEG_VERSION=${FFMPEG}-${CONFIG_FFMPEG_VERSION}
 }
 
-download_package_for_ffmpeg () {
+get_ffmpeg () {
     set_ffmpeg
     if [ "$X264_FOR_FFMPEG" = "yes" ];then
         get_x264
@@ -48,23 +52,9 @@ download_package_for_ffmpeg () {
     if [ "$X265_FOR_FFMPEG" = "yes" ];then
         get_x265
     fi
-    tget https://ffmpeg.org//releases/${FFMPEG_VERSION}.tar.bz2
+    tget_package_from_arch  $FFMPEG_ARCH_PATH $ARCHIVE_PATH/$FFMPEG_FILE_NAME  https://ffmpeg.org/releases/$FFMPEG_FILE_NAME
 }
 
-## OTHER_LIB=${OUTPUT_PATH}/__all_without_ffmpeg
-## prepare_other_lib_for_ffmpeg () {
-##     # 这一个是针对 ffmpeg 方便管理外部库使用的
-##     # 核心思想是把 所有的库都放到一起，再让 ffmpeg ld的时候在这里找（而不是添加多行） --extra-cflags="-I${X264_DIR}/include -I${xxx}/include" \
-##     cd ${BASE}/install/
-##     rm ${OTHER_LIB} -rf
-##     ls > /tmp/list.txt
-##     mkdir ${OTHER_LIB} -p
-##     for sub_dir in `cat /tmp/list.txt`
-##     do
-##         cp ${sub_dir}/* ${OTHER_LIB} -r -v
-##     done
-##     rm -rf /tmp/list.txt
-## }
 function gen_ffmpeg_make_sh () {
     local for_host="$1"
     local enable_for_x264=""
@@ -149,7 +139,7 @@ function mk_ffmpeg() {
 function make_ffmpeg ()
 {
     require cmake || return 1
-    download_package_for_ffmpeg  || return 1
+    get_ffmpeg  || return 1
     tar_package || return 1
     if [ "$X264_FOR_FFMPEG" = "yes" ];then
         make_x264 || return 1
@@ -172,7 +162,7 @@ function mk_ffmpeg_host() {
 function make_ffmpeg_host ()
 {
     require cmake || return 1
-    download_package_for_ffmpeg  || return 1
+    get_ffmpeg  || return 1
     tar_package || return 1
     if [ "$X264_FOR_FFMPEG" = "yes" ];then
         make_x264_host || return 1
--- ffmpeg/make.sh
+++ ffmpeg/make.sh
@@ -12,34 +12,21 @@ source ../.common
 #export CONFIG_X265_VERSION=3.5
 
 
-########### 指定输出路径
-#export X264_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
-#export X265_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
-#export FFMP_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
+########### 指定安装路径
+#export FFMPEG_OUTPUT_PATH=${OUTPUT_PATH}/ffmpeg
+export X264_OUTPUT_PATH=${FFMPEG_OUTPUT_PATH}
+export X265_OUTPUT_PATH=${FFMPEG_OUTPUT_PATH}
 
-#export X264_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
-#export X265_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
 #export FFMPEG_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/ffmpeg
+export X264_OUTPUT_PATH_HOST=${FFMPEG_OUTPUT_PATH_HOST}
+export X265_OUTPUT_PATH_HOST=${FFMPEG_OUTPUT_PATH_HOST}
 
 
 
-############ 鉴于gcc版本可能存在差异，USING_X264_ASM可能需要做调整才能编译x264
 # LIBX264 Config(启用libx264时有效)
-### 通过y/n来配置libx264是否启用ASM（默认禁用）
-#export USING_X264_ASM=y
 ### 通过y/n来配置libx264是否启用OPENCL（默认禁用）
 #export USING_X264_OPENCL=n
 
 make_ffmpeg
-
-
-
-############ 鉴于gcc版本可能存在差异，USING_X264_ASM可能需要做调整才能编译x264
-
-# LIBX264 Config(启用libx264时有效)
-### 通过y/n来配置libx264是否启用ASM（默认禁用）
-#export USING_X264_ASM=n
-### 通过y/n来配置libx264是否启用OPENCL（默认禁用）
-#export USING_X264_OPENCL=n
 
 #make_ffmpeg_host
